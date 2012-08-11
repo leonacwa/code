c4958170856fc0b50fb8d4ff90cae170ba86bb5a
leonacwa <leon.acwa@gmail.com>
2012-08-11 14:57:43 +0800

    不再自动添加所有内容，防止目录也更新commit-message

--- update.sh
+++ update.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+echo "Please do it by yourself"
+echo "Command;"
+
+echo "git add ."
+echo "git commit -m <message>"
+echo "git push origin master"
+echo ""
+exit
+
 if [ "$#" != "1" ]; then
 	echo "Empty <commit mesage>"
 	echo "Usage: $0 <commit message>"
 	exit
 fi
+
+
 git add .
 git commit -m "$1"
 git push origin master
+
