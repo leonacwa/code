9d9e7a9e9c15f9f60cc02735c5f7198924b5f575
leonacwa <leon.acwa@gmail.com>
2012-08-25 21:29:50 +0800

    递推类型的题

---
+++ recusion/hdu4260_The End of The World.cpp
@@ -0,0 +1,24 @@
+/* The End of The World
+ * 汉诺塔问题的变形
+ * */
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+typedef long long int64;
+
+char str[65];
+
+int64 dfs(char A, char B, char C, int n) {
+	if (n == -1) return 0LL;
+	if (str[n] == A) return dfs(A, C, B, n-1) + (1LL << n);
+	else return dfs(C, B, A, n-1);
+}
+
+int main() {
+	while (cin >> str) {
+		if (str[0] == 'X') break;
+		cout << dfs('A', 'B', 'C', strlen(str)-1) << endl;
+	}
+	return 0;
+}
